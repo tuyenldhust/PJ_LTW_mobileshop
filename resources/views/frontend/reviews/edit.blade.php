61e2b10928a7e795053c10a80a6c586b00d669d7
nguyettron2001@gmail.com <nguyet>
2023-02-10 19:21:02 +0700

    add-review

--- resources/views/frontend/reviews/edit.blade.php
+++ resources/views/frontend/reviews/edit.blade.php
@@ -3,22 +3,22 @@
 @section('title', "Edit your Review")
 
 @section('content')
-    <div class="container py-5">
-        <div class="row">
-            <div class="col-md-12">
+    <div class="wrapper-review">
                 <div class="card">
                     <div class="card-body">
-                        <h5>You are writing a review for {{ $review->product->name }}</h5>
-                        <form action="{{ url('/update-review') }}" method="POST">
-                            @csrf
-                            @method('PUT')
-                            <input type="hidden" name="review_id" value="{{ $review->id }}">
-                            <textarea class="form-control" name="user_review" rows="5" placeholder="Write a review">{{ $review->user_review }}</textarea>
-                            <button type="submit" class="btn btn-primary mt-3">Update Review</button>
-                        </form>
+                            <div class="margin-top">
+                                    <h5>You are writing a review for {{ $review->product->name }}</h5>
+                            </div>
+                            <div class="form-text margin-top">
+                                <form class="form-body" action="{{ url('/update-review') }}" method="POST">
+                                @csrf
+                                @method('PUT')
+                                    <input type="hidden" name="review_id" value="{{ $review->id }}">
+                                    <textarea class="form-control form-review" name="user_review" rows="5" placeholder="Write a review">{{ $review->user_review }}</textarea>
+                                    <button type="submit" class="btn-submit">Update Review</button>
+                                </form>
+                            </div>
                     </div>
                 </div>
-            </div>
-        </div>
     </div>
 @endsection
--- resources/views/frontend/reviews/index.blade.php
+++ resources/views/frontend/reviews/index.blade.php
@@ -3,23 +3,32 @@
 @section('title', "Write a Review")
 
 @section('content')
-    <div class="container py-5">
-        <div class="row">
-            <div class="col-md-12">
+    <div class="wrapper-review">
                 <div class="card">
                     <div class="card-body">
                         @if ($verified_purchase->count() > 0)
-                            <h5>You are writing a review for {{ $product->name }}</h5>
-                            <form action="{{ url('/add-review') }}" method="POST">
+                            <div><p style="font-size :18px;">Đánh giá sản phẩm</p></div>
+                            <div class="product-review-top margin-top">
+                                <div class="pr-img">
+                                    <img src="{{ asset('assets/uploads/products/'.$product->image) }}" width="50px" heigth="40px" alt="">
+                                </div>
+                                <div>
+                                    <h5>{{ $product->name }}</h5>
+                                </div>
+                            </div>
+                            <div class="form-text margin-top">
+                                <form class="form-body" action="{{ url('/add-review') }}" method="POST">
                                 @csrf
-                                <input type="hidden" name="product_id" value="{{ $product->id }}">
-                                <textarea class="form-control" name="user_review" rows="5" placeholder="Write a review"></textarea>
-                                <button type="submit" class="btn btn-primary mt-3">Submit Review</button>
-                            </form>
+                                    <input type="hidden" name="product_id" value="{{ $product->id }}">
+                                    <textarea class="form-control form-review" name="user_review" rows="5" placeholder="Please leave a review"></textarea>
+                                    <button type="submit" class="btn-submit">Submit Review</button>
+                                </form>
+                            </div>
+                           
                         @else
-                            <div class="alert alert-danger">
-                                <h5>Bạn không đủ điều kiện để đánh giá sản phẩm này!</h5>
-                                <p>
+                            <div class="alert alert-danger ">
+                                <h4 style="margin-bottom:15px">Bạn không đủ điều kiện để đánh giá sản phẩm này!</h4>
+                                <p style="margin-bottom:15px">
                                 Đối với mức độ đáng tin cậy của các bài đánh giá,
                                 chỉ những khách hàng đã mua sản phẩm mới có thể viết bài đánh giá về sản phẩm.
                                 </p>
@@ -28,7 +37,5 @@
                         @endif
                     </div>
                 </div>
-            </div>
-        </div>
     </div>
 @endsection
